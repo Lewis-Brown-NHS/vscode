1ec4e5b233ee5aeb02d241e0efc1c31c9b58500a
Chris <cgerke@outlook.com>
2022-08-21 10:50:21 +0800

    Modularise

--- .vscode/Global.ps1
+++ .vscode/Global.ps1
@@ -3,6 +3,7 @@
 [string]$WDADesktop = "C:\Users\WDAGUtilityAccount\Desktop"
 [string]$Application = "$(& git branch --show-current)"
 [string]$Cache = "$env:ProgramData\win32app\$Application"
+[string]$LogonCommand = "LogonCommand.ps1"
 
 # Cache resources
 Remove-Item -Path "$Cache" -Recurse -Force -ErrorAction Ignore
