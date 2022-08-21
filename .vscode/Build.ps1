1a20312d3eaafdacd7760ebf8f3f2d258cfa37d4
Chris <cgerke@outlook.com>
2022-08-21 10:47:39 +0800

    Modularise

--- .vscode/Build.ps1
+++ .vscode/Build.ps1
@@ -22,23 +22,3 @@ Start-Process @processOptions
 # Rename and prepare for upload
 Move-Item -Path "$env:TEMP\install.intunewin" -Destination "$Desktop\$Application.intunewin" -Force -Verbose
 explorer $Desktop
-
-# Prepare Sandbox
-@"
-<Configuration>
-<Networking>Enabled</Networking>
-<MappedFolders>
-    <MappedFolder>
-    <HostFolder>$env:ProgramData\win32app</HostFolder>
-    <SandboxFolder>$WDADesktop</SandboxFolder>
-    <ReadOnly>true</ReadOnly>
-    </MappedFolder>
-</MappedFolders>
-<LogonCommand>
-    <Command>powershell -executionpolicy unrestricted -command "Start-Process powershell -ArgumentList ""-nologo -file $WDADesktop\Sandbox.ps1"""</Command>
-</LogonCommand>
-</Configuration>
-"@ | Out-File "$env:ProgramData\win32app\$Application.wsb"
-
-# Execute Sandbox
-Start-Process explorer -ArgumentList "$env:ProgramData\win32app\$Application.wsb" -Verbose
